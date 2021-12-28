e19753cfa53bcc3f57fd72635c470ebf274e9975
froliceric0914 <wzhao.eric@gmail.com>
2021-12-28 17:41:27 -0500

    associate User and Compnany in graphQL schema

--- schema/schema.js
+++ schema/schema.js
@@ -2,12 +2,30 @@ const graphql = require('graphql');
 const axios = require('axios');
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: { type: GraphQLString }, //id is a string?
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+  },
+});
+
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
+    //Nested Queries
+    company: {
+      type: CompanyType,
+      resolve(parentValue, agrs) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then((res) => res.data);
+      },
+    },
   },
 });
 //
