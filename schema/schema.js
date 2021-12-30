62edfbe667f4bfa5d5c29c726440a7fadb1ab003
froliceric0914 <wzhao.eric@gmail.com>
2021-12-29 20:16:51 -0500

    add mutation methods for user

--- schema/schema.js
+++ schema/schema.js
@@ -6,6 +6,7 @@ const {
   GraphQLInt,
   GraphQLSchema,
   GraphQLList,
+  GraphQLNonNull,
 } = graphql;
 
 const CompanyType = new GraphQLObjectType({
@@ -45,6 +46,7 @@ const UserType = new GraphQLObjectType({
   }),
 });
 
+//the query name that can be utilize by the graphiQL
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
@@ -70,6 +72,53 @@ const RootQuery = new GraphQLObjectType({
   },
 });
 
+//different ObejectType basically have different resolve funtion
+
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addUser: {
+      type: UserType, // sometimes it will return to another typed
+      args: {
+        firstName: { type: new GraphQLNonNull(GraphQLString) }, // low-level validation
+        age: { type: new GraphQLNonNull(GraphQLInt) },
+        companyId: { type: GraphQLString },
+      },
+      resolve(parentValue, { firstName, age }) {
+        return axios
+          .post('http://localhost:3000/users', { firstName, age })
+          .then((res) => res.data);
+      },
+    },
+    editUser: {
+      type: UserType, // sometimes it will return to another typed
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        firstName: { type: GraphQLString }, // low-level validation
+        age: { type: GraphQLInt },
+        companyId: { type: GraphQLString },
+      },
+      resolve(parentValue, args) {
+        return axios
+          .patch(`http://localhost:3000/users/${args.id}`, args) //patch only update patially/fully of the field(s) input
+          .then((res) => res.data);
+      },
+    },
+    deleteUser: {
+      type: UserType, // sometimes it will return to another typed
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) }, // low-level validation
+      },
+      resolve(parentValue, args) {
+        return axios
+          .delete(`http://localhost:3000/users/${args.id}`)
+          .then((res) => res.data);
+      },
+    },
+  },
+});
+
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation,
 });
